5e22837781219f2c1d741a273fd42c2d8c8c0f87
KappitaRoss <humbertocanalesmtz@gmail.com>
2020-08-09 19:31:46 -0500

    Pruebas parte 4

--- index.php
+++ index.php
@@ -3,10 +3,12 @@ $componentes_url = parse_url($_SERVER["REQUEST_URI"]);
 $ruta = $componentes_url['path'];
 $partes_ruta = explode('/', $ruta);
 $partes_ruta = array_filter($partes_ruta);
+$partes_ruta = array_slice($partes_ruta, 0);
+$partes_ruta = array_slice($partes_ruta, 1);
 
 $ruta_elegida = "views/404.php";
 
-if($partes_ruta[0] == 'http://34.230.52.161'){
+if($partes_ruta[0] == '34.230.52.161'){
     if(count($partes_ruta) == 1){
         $ruta_elegida = "views/home.php";
     } else if(count($partes_ruta) == 2){
